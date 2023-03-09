dd2b76b2c7fb1aab8a366b6ee94de9241ba70961
Chamodh <IT20189044@my.sliit.lk>
2023-03-09 21:33:56 +0530

    Functionality fully completed

--- NEUTRON_Ecommerce/screens/ViewCartScreen.tsx
+++ NEUTRON_Ecommerce/screens/ViewCartScreen.tsx
@@ -10,11 +10,18 @@ import ModalButton from '../components/atoms/buttons/ModalButton';
 import FormGroupWithIcon from '../components/molecules/FormGroupWithIcon';
 import CartCard from '../components/molecules/CartCard';
 import { Iphone } from '../assets/image';
+import HeadLine4 from '../components/atoms/typographies/HeadLine4';
 
 export default function ViewCart() {
   const theme = useTheme();
   const style = useThemedStyles(styles);
   const array = [1, 2, 3, 4, 5];
+  const [totalPrice, setTotalPrice] = useState(0);
+  let itemPrice = 420000;
+
+  function calculateTotal(itemTotalPrice: number) {
+    setTotalPrice(totalPrice + itemTotalPrice);
+  }
 
   return (
     <SafeAreaView style={style.container}>
@@ -35,12 +42,54 @@ export default function ViewCart() {
               description={
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor adipiscing elit, sed do eiusmod tempor..'
               }
-              price={'420,000.00'}
+              price={itemPrice}
               image={Iphone}
+              calcTotal={calculateTotal}
             />
           );
         })}
       </ScrollView>
+      <View style={style.row}>
+        <HeadLine4
+          value={i18n.t('viewCartPage.subTotal')}
+          color={theme.COLORS.PRIMARY}
+          marginLeft={10}
+        />
+        <Paragraph
+          value={`(2 ${i18n.t('viewCartPage.items')})`}
+          color={theme.COLORS.PRIMARY}
+          marginLeft={10}
+          marginTop={3}
+        />
+        <HeadLine4
+          marginLeft={105}
+          value={`LKR.${totalPrice}`}
+          marginTop={3}
+          color={theme.COLORS.PRIMARY}
+        />
+      </View>
+      <View
+        style={{
+          borderStyle: 'dotted',
+          borderWidth: 1,
+          borderRadius: 1,
+          borderColor: theme.COLORS.BLACK,
+          width: '90%'
+        }}
+      />
+      <View style={style.row}>
+        <HeadLine4
+          value={i18n.t('viewCartPage.totalAmount')}
+          color={theme.COLORS.PRIMARY}
+          marginLeft={10}
+        />
+        <HeadLine4
+          marginLeft={130}
+          value={`LKR.${totalPrice}`}
+          marginTop={3}
+          color={theme.COLORS.PRIMARY}
+        />
+      </View>
       <ModalButton
         value={i18n.t('viewCartPage.checkoutBtn')}
         color={theme.COLORS.PRIMARY}
@@ -84,5 +133,11 @@ const styles = (theme: {
       backgroundColor: theme.COLORS.WHITE
     },
     imageStyle: { height: 100, width: 150 },
-    column: { flexDirection: 'column' }
+    column: { flexDirection: 'column' },
+    row: {
+      flexDirection: 'row',
+      alignSelf: 'flex-start',
+      marginTop: 20,
+      marginBottom: 20
+    }
   });
