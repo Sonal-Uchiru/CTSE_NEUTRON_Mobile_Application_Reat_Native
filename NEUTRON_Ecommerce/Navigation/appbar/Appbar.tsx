b6805b63162f604452f96e284446b452fb37c931
sonaljayawardana <it20190484@my.sliit.lk>
2023-03-23 19:06:09 +0530

    logout service implemented

--- NEUTRON_Ecommerce/Navigation/appbar/Appbar.tsx
+++ NEUTRON_Ecommerce/Navigation/appbar/Appbar.tsx
@@ -85,7 +85,6 @@ export default function AppHeader() {
               ) : (
                 <></>
               )}
-
               <Menu.Item
                 leadingIcon="logout"
                 onPress={() => {
